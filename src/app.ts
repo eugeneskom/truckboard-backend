c4bf10d70b214e5aab040c16a55a75820613d546
Eugene <eugeneskom@gmail.com>
2024-10-10 06:29:52 +0300

    removed posting routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import carrierListRoutes from './routes/carrierList'
+import carrierListRoutes from './routes/carrierList';
 import truckListRouter from './routes/truckList';
 import driverListRouter from './routes/driverList';
 import searchListRouter from './routes/searchList';
 import rateListRouter from './routes/rateList'
 import agentListRouter from './routes/agentList';
-// import postingRouter from './routes/postings'
 import addRoutes from './routes/addEntities'
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import AuthRouter from './routes/auth'
-import AggregatedRoute from './routes/aggregatedData'
-import updateRoute from './routes/updateData'
+import AuthRouter from './routes/auth';
+import AggregatedRoute from './routes/aggregatedData';
+import updateRoute from './routes/updateData';
 dotenv.config();
 
 
@@ -20,14 +19,13 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow only this origin
+  origin: 'http://localhost:3000', // Allow only this origin in development
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // Enable cookies and authorization headers
+  credentials: true, // Enables cookies and authorization headers
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // 
-
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -54,8 +52,6 @@ app.use('/api/searches', searchListRouter);
 
 app.use('/api/rates', rateListRouter);
 
-// app.use('/api/postings', postingRouter);
-
 app.use('/api/add', addRoutes);
 
 app.use('/api/auth', AuthRouter);
@@ -63,7 +59,6 @@ app.use('/api/auth', AuthRouter);
 app.use('/api/aggregated', AggregatedRoute);
 
 app.use('/api/update-data', updateRoute);
-
 
 
 export default app;
