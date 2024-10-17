639cb1232e51fdfe89932b1c5643d8ad3c009362
eugeneskom <eugeneskom@gmail.com>
2024-10-17 15:15:33 +0300

    changed ws port

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import app from './app';
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
-const WS_PORT = process.env.WS_PORT || 3002;
+const WS_PORT = process.env.WS_PORT || 3001;
 
 // Create a separate WebSocket server
 const wss = new WebSocket.Server({ port: Number(WS_PORT) });
